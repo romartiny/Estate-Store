d71909f2c791b10a93fa67dd0ce540c6815c7abe
romartiny <91370772+romartiny@users.noreply.github.com>
2022-01-24 20:14:06 +0300

    update js

--- js/modules/scripts.js
+++ js/modules/scripts.js
@@ -14,187 +14,188 @@ const ONE_DAY = 1000 * 60 * 60 * 24;
 const TWO_DAYS = ONE_DAY * 2;
 
 const names = [
-    "Двушка в центре Питера",
-    "Однушка в спальнике Питера",
-    "Трешка рядом с Кремлём",
-    "Студия для аскетов",
-    "Апартаменты для фрилансера"
+  "Двушка в центре Питера",
+  "Однушка в спальнике Питера",
+  "Трешка рядом с Кремлём",
+  "Студия для аскетов",
+  "Апартаменты для фрилансера"
 ];
 
 const descriptions = [
-    "Студия с лаконичным дизайном возле Ангары.",
-    "Трёхкомнатная квартира для большой семьи рядом с Кремлём.",
-    "2 минуты до набережной и прекрасного вида на Волгу.",
-    "В квартире есть сауна, джакузи и домашний кинотеатр. Перепланировка согласована.",
-    "Уютная однушка в тихом спальном районе. Рядом лес и озёра."
+  "Студия с лаконичным дизайном возле Ангары.",
+  "Трёхкомнатная квартира для большой семьи рядом с Кремлём.",
+  "2 минуты до набережной и прекрасного вида на Волгу.",
+  "В квартире есть сауна, джакузи и домашний кинотеатр. Перепланировка согласована.",
+  "Уютная однушка в тихом спальном районе. Рядом лес и озёра."
 ];
 
 const sellers = [
-    "Бюро Семёна",
-    "Игнат-Агент",
-    "Виталий Петрович",
-    "Марья Андреевна"
+  "Бюро Семёна",
+  "Игнат-Агент",
+  "Виталий Петрович",
+  "Марья Андреевна"
 ];
 
 const cities = [
-    "Иркутск",
-    "Москва",
-    "Красноярск",
-    "Минск"
+  "Иркутск",
+  "Москва",
+  "Красноярск",
+  "Минск"
 ];
 
 const streets = [
-    "ул. Шахтеров",
-    "ул. Полярная",
-    "ул. Лиственная",
-    "ул. Мира",
-    "ул. Советская"
+  "ул. Шахтеров",
+  "ул. Полярная",
+  "ул. Лиственная",
+  "ул. Мира",
+  "ул. Советская"
 ];
 
 const fileNames = [
-    "apt_1.png",
-    "apt_2.png",
-    "apt_3.png",
-    "apt_4.png",
-    "apt_5.png",
-    "apt_6.png",
-    "house_1.png",
-    "house_2.png",
-    "house_3.png",
-    "house_4.png",
+  "apt_1.png",
+  "apt_2.png",
+  "apt_3.png",
+  "apt_4.png",
+  "apt_5.png",
+  "apt_6.png",
+  "house_1.png",
+  "house_2.png",
+  "house_3.png",
+  "house_4.png",
 ];
 
 const types = [
-    "house",
-    "apartment",
-    "flat"
+  "house",
+  "apartment",
+  "flat"
 ];
 
 const estates = [];
 
 const months = [
-    'января',
-    'февраля',
-    'марта',
-    'апреля',
-    'мая',
-    'июня',
-    'июля',
-    'августа',
-    'сентября',
-    'октября',
-    'ноября',
-    'декабря',
+  'января',
+  'февраля',
+  'марта',
+  'апреля',
+  'мая',
+  'июня',
+  'июля',
+  'августа',
+  'сентября',
+  'октября',
+  'ноября',
+  'декабря',
 ];
 
 const getRandomNumber = (minNum, maxNum) => {
-    return Math.floor(Math.random() * (maxNum - minNum) + minNum);
+  return Math.floor(Math.random() * (maxNum - minNum) + minNum);
 }
 
 const getName = (arr) => {
-    const i = getRandomNumber(0, arr.length);
-    return arr[i];
+  const i = getRandomNumber(0, arr.length);
+  return arr[i];
 }
 
 const getDescription = (arr) => {
-    const i = getRandomNumber(0, arr.length);
-    return arr[i];
+  const i = getRandomNumber(0, arr.length);
+  return arr[i];
 }
 
 const getPrice = (MIN_PRICE, MAX_PRICE) => {
-    return (getRandomNumber(MIN_PRICE, MAX_PRICE) / 100) * 100;
+  return (getRandomNumber(MIN_PRICE, MAX_PRICE) / 100) * 100;
 }
 
 const getSellerName = (arr) => {
-    const index = getRandomNumber(0, arr.length);
-    return arr[index];
+  const index = getRandomNumber(0, arr.length);
+  return arr[index];
 }
 
 const getRating = (MIN_RATING, MAX_RATING) => {
-    return getRandomNumber(MIN_RATING, MAX_RATING) / 10;
+  return getRandomNumber(MIN_RATING, MAX_RATING) / 10;
 }
 
 const getPublishDate = () => {
-    const dateNow = Date.now();
-    const date = getRandomNumber(dateNow - ONE_DAY * 5, dateNow);
-    return date;
+  const dateNow = Date.now();
+  const date = getRandomNumber(dateNow - ONE_DAY * 5, dateNow);
+  return date;
 }
 
 const getCity = (arr) => {
-    const i = getRandomNumber(0, arr.length);
-    return arr[i];
+  const i = getRandomNumber(0, arr.length);
+  return arr[i];
 }
 
 const getStreet = (arr) => {
-    const i = getRandomNumber(0, arr.length);
-    return arr[i];
+  const i = getRandomNumber(0, arr.length);
+  return arr[i];
 }
 
 const getBuildNumber = () => {
-    return Math.floor((Math.random() * 40));
+  return Math.floor((Math.random() * 40));
 }
 
 const renderPhotosSrc = () => {
-    const count = getRandomNumber(1, 4);
-    const photos = [];
+  const count = getRandomNumber(1, 4);
+  const photos = [];
 
-    for (let index = 0; index < count; index++) {
-        const photoName = fileNames[getRandomNumber(0, fileNames.length)];
-        const srcPhoto = `img/${photoName}`;
-        photos.push(srcPhoto);
-    }
+  for (let index = 0; index < count; index++) {
+    const photoName = fileNames[getRandomNumber(0, fileNames.length)];
+    const srcPhoto = `img/${photoName}`;
+    photos.push(srcPhoto);
+  }
 
-    return photos;
+  return photos;
 }
 
 const getType = (arr) => {
-    const i = getRandomNumber(0, arr.length);
-    return arr[i];
+  const i = getRandomNumber(0, arr.length);
+  return arr[i];
 }
 
 const getArea = (MIN_AREA, MAX_AREA) => {
-    return getRandomNumber(MIN_AREA, MAX_AREA);
+  return getRandomNumber(MIN_AREA, MAX_AREA);
 }
 
 const getRoomsCount = (MIN_ROOM_COUNT, MAX_ROOM_COUNT) => {
-    return getRandomNumber(MIN_ROOM_COUNT, MAX_ROOM_COUNT);
+  return getRandomNumber(MIN_ROOM_COUNT, MAX_ROOM_COUNT);
 }
 
-const getEstate = () => {
-    return {
-        name: getName(names),
-        description: getDescription(descriptions),
-        price: getPrice(MIN_PRICE, MAX_PRICE),
-        category: CATEGORY,
-        seller: {
-            fullname: getSellerName(sellers),
-            rating: getRating(MIN_RATING, MAX_RATING),
-        },
-        publishDate: getPublishDate(),
-        address: {
-            city: getCity(cities),
-            street: getStreet(streets),
-            building: getBuildNumber(),
-        },
-        photos: renderPhotosSrc(fileNames),
-        filters: {
-            type: getType(types),
-            area: getArea(MIN_AREA, MAX_AREA),
-            roomsCount: getRoomsCount(MIN_ROOM_COUNT, MAX_ROOM_COUNT),
-        },
-    };
+const getEstate = (index) => {
+  return {
+    id: index,
+    name: getName(names),
+    description: getDescription(descriptions),
+    price: getPrice(MIN_PRICE, MAX_PRICE),
+    category: CATEGORY,
+    seller: {
+      fullname: getSellerName(sellers),
+      rating: getRating(MIN_RATING, MAX_RATING),
+    },
+    publishDate: getPublishDate(),
+    address: {
+      city: getCity(cities),
+      street: getStreet(streets),
+      building: getBuildNumber(),
+    },
+    photos: renderPhotosSrc(fileNames),
+    filters: {
+      type: getType(types),
+      area: getArea(MIN_AREA, MAX_AREA),
+      roomsCount: getRoomsCount(MIN_ROOM_COUNT, MAX_ROOM_COUNT),
+    },
+  };
 }
 
 for (let i = 0; i < COUNT_ESTATE; i++) {
-    const estate = getEstate();
-    estates.push(estate);
+  const estate = getEstate(i);
+  estates.push(estate);
 
 }
 
 const productList = document.querySelector('.results__list');
 
 const getProductItem = (item) => {
-    const card = `<li class="results__item product">
+  const card = `<li class="results__item product">
     <button class="product__favourite fav-add" type="button" aria-label="Добавить в избранное">
       <svg width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" clip-rule="evenodd" d="M3 7C3 13 10 16.5 11 17C12 16.5 19 13 19 7C19 4.79086 17.2091 3 15 3C12 3 11 5 11 5C11 5 10 3 7 3C4.79086 3 3 4.79086 3 7Z" stroke="white" stroke-width="2" stroke-linejoin="round"/>
@@ -202,72 +203,84 @@ const getProductItem = (item) => {
     </button>
     <div class="product__image">
       <div class="product__image-more-photo hidden">+2 фото</div>
-      <img src="${item.photos[0]}" width="318" height="220" alt="${item.name}">
+      <img src="${item.photos[0]}" width="318" height="220" alt="${item.name}" data-id="${item.id}">
     </div>
     <div class="product__content">
       <h3 class="product__title">
-        <a href="#">${item.name}</a>
+        <a href="#" data-id="${item.id}">${item.name}</a>
       </h3>
-      <div class="product__price">${item.price}</div>
+      <div class="product__price">${item.price} ₽</div>
       <div class="product__address">${item.address.street}</div>
       <div class="product__date">${getProductDate(item.publishDate)}</div>
     </div>
   </li>`;
-    return card;
+  return card;
 }
 
 const getProductDate = (publishDate) => {
-    const dateNow = Date.now();
-    const dateDifference = dateNow - publishDate;
+  const dateNow = Date.now();
+  const dateDifference = dateNow - publishDate;
 
-    if (dateDifference < ONE_DAY) {
-        return 'Сегодня';
-    } else if (dateDifference > ONE_DAY && dateDifference < TWO_DAYS) {
-        return 'Вчера';
-    } else {
-        return `${new Date (publishDate).getDate()} ${months[new Date (publishDate).getMonth()]} ${new Date (publishDate).getFullYear()} год`;
-    }
+  if (dateDifference < ONE_DAY) {
+    return 'Сегодня';
+  } else if (dateDifference > ONE_DAY && dateDifference < TWO_DAYS) {
+    return 'Вчера';
+  } else {
+    return `${new Date (publishDate).getDate()} ${months[new Date (publishDate).getMonth()]} ${new Date (publishDate).getFullYear()} год`;
+  }
 }
 
 const renderElement = (template) => {
-    const item = document.createElement('div');
-    item.innerHTML = template;
-    return item.firstChild;
+  const item = document.createElement('div');
+  item.innerHTML = template;
+  return item.firstChild;
 };
 
-const renderProductList = () => {
-    const fragment = document.createDocumentFragment();
+const renderProductList = (list) => {
+  const fragment = document.createDocumentFragment();
+  //
+  list.forEach((item) => {
+    const productItem = getProductItem(item);
+    const element = renderElement(productItem);
+    fragment.appendChild(element);
+  });
 
-    const list = estates.length < 7 ? estates.slice() : estates.slice(0, 7); // тернарный оператор
-
-    list.forEach((item) => {
-        const productItem = getProductItem(item);
-        const element = renderElement(productItem);
-        fragment.appendChild(element);
-    });
-
-    return fragment;
+  return fragment;
 }
+
+const findProduct = (id, list) => {
+  return list.find((el) => el.id === id);
+}
+
+const list = estates.length < 7 ? estates.slice() : estates.slice(0, 7);
 
 productList.innerHTML = "";
 
-productList.appendChild(renderProductList());
+productList.appendChild(renderProductList(list));
 
+// init clicks & buttons
 const modal = document.querySelector('.popup');
 const titleClick = document.querySelectorAll('.product__title');
 const imageClick = document.querySelectorAll('.product__image');
-const closePopup = document.querySelector('.popup__close');
+const closePopup = document.querySelectorAll('.popup__close');
+
+const closePopupInit = () => {
+  const closePopup = document.querySelectorAll('.popup__close');
+  console.log(closePopup);
+  return closePopup;
+}
+//render modal window
 
 const getModalWindow = (item) => {
-    const card = `<div class="popup__inner">
+  const card = `<div class="popup__inner">
     <button class="popup__close" type="button" aria-label="Закрыть">
       <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" clip-rule="evenodd" d="M0.292893 0.292893C0.683418 -0.0976311 1.31658 -0.0976311 1.70711 0.292893L8 6.58579L14.2929 0.292893C14.6834 -0.0976311 15.3166 -0.0976311 15.7071 0.292893C16.0976 0.683418 16.0976 1.31658 15.7071 1.70711L9.41421 8L15.7071 14.2929C16.0976 14.6834 16.0976 15.3166 15.7071 15.7071C15.3166 16.0976 14.6834 16.0976 14.2929 15.7071L8 9.41421L1.70711 15.7071C1.31658 16.0976 0.683418 16.0976 0.292893 15.7071C-0.0976311 15.3166 -0.0976311 14.6834 0.292893 14.2929L6.58579 8L0.292893 1.70711C-0.0976311 1.31658 -0.0976311 0.683418 0.292893 0.292893Z"/>
       </svg>
     </button>
-    <div class="popup__date">Сегодня</div>
-    <h3 class="popup__title">1231313</h3>
-    <div class="popup__price">21313 ₽</div>
+    <div class="popup__date">${getProductDate(item.publishDate)}</div>
+    <h3 class="popup__title">${item.name}</h3>
+    <div class="popup__price">${item.price} ₽</div>
     <div class="popup__columns">
       <div class="popup__left">
         <div class="popup__gallery gallery">
@@ -277,7 +290,7 @@ const getModalWindow = (item) => {
             </svg>
           </button>
           <div class="gallery__main-pic">
-            <img src="img/house_1.png" width="520" height="340" alt="Загородный дом">
+            <img src="${item.photos[0]}" width="520" height="340" alt="Загородный дом">
           </div>
           <ul class="gallery__list">
             <li class="gallery__item gallery__item--active">
@@ -297,107 +310,129 @@ const getModalWindow = (item) => {
         <ul class="popup__chars chars">
           <li class="chars__item">
             <div class="chars__name">Площадь</div>
-            <div class="chars__value">105</div>
+            <div class="chars__value">${item.filters.area}</div>
           </li>
           <li class="chars__item">
             <div class="chars__name">Количество комнат</div>
-            <div class="chars__value">2</div>
+            <div class="chars__value">${item.filters.roomsCount}</div>
           </li>
           <li class="chars__item">
             <div class="chars__name">Тип недвижимости</div>
-            <div class="chars__value">дом</div>
+            <div class="chars__value">${item.filters.type}</div>
           </li>
         </ul>
         <div class="popup__seller seller seller--good">
           <h3>Продавец</h3>
           <div class="seller__inner">
-            <a class="seller__name" href="#">Игнат-Агент</a>
-            <div class="seller__rating"><span>4.9</span></div>
+            <a class="seller__name" href="#">${item.seller.fullname}</a>
+            <div class="seller__rating"><span>${item.seller.rating}</span></div>
           </div>
         </div>
         <div class="popup__description">
           <h3>Описание товара</h3>
-          <p>Деревянный дом на берегу озера. На первом этаже кухня-гостиная, на втором этаже спальня. Идеально для пары без детей. В доме много окон, поэтому много света. Дизайнерская мебель входит в стоимость дома.</p>
+          <p>${item.description}</p>
         </div>
       </div>
       <div class="popup__right">
         <div class="popup__map">
           <img src="img/map.jpg" width="268" height="180" alt="Москва, Нахимовский проспект, дом 5">
         </div>
-        <div class="popup__address">Москва, Нахимовский проспект, дом 5</div>
+        <div class="popup__address">${item.address.city} ${item.address.street} ${item.address.building}</div>
       </div>
     </div>
   </div>`;
-    return card;
+  return card;
 }
 
+//buttons for open and close
+
 const closeModal = () => {
-    modal.classList.remove('popup--active');
+  modal.classList.remove('popup--active');
 }
 
 const openModal = () => {
-    modal.classList.add('popup--active');
+  modal.classList.add('popup--active');
 }
+
+//listner key presses
 
 const initModalListeners = () => {
+  document.addEventListener('click', (evt) => {
+    if (evt.target === modal) {
+      evt.preventDefault;
+      closeModal();
+    }
+  });
 
-    closePopup.addEventListener('click', () => {
-        closeModal();
-    })
+  document.addEventListener('keypress', (evt) => {
+    if (evt.key === 'Escape') {
+      closeModal();
+    }
+  });
+
+  document.addEventListener('keypress', (evt) => {
+    if (evt.key === 'Enter') {
+      closeModal();
+    }
+  });
 }
 
+//on click do
+
+
 const onProductCardTitleClick = (evt) => {
-    evt.preventDefault();
-    openModal();
+  const id = +evt.target.dataset.id;
+  const productData = findProduct(id, list);
+  renderModalInfo(productData);
+  closePopupInit();
+  initModalListeners();
+  openModal();
 }
 
 const onProductCardImageClick = (evt) => {
-    openModal();
-    initModalListeners();
-    renderModalInfo();
+  // console.log(evt.target.dataset.id);
+  const id = +evt.target.dataset.id;
+  const productData = findProduct(id, list);
+  renderModalInfo(productData);
+  closePopupInit();
+  initModalListeners();
+  openModal();
 }
 
+const onClosePopupClick = (evt) => {
+  initModalListeners();
+  closeModal();
+  console.log('close');
+}
+
+//on click
+
 titleClick.forEach((button) => {
-    button.addEventListener('click', onProductCardTitleClick);
+  button.addEventListener('click', onProductCardTitleClick);
+});
+
+closePopup.forEach((button) => {
+  button.addEventListener('click', onClosePopupClick);
 });
 
 imageClick.forEach((image) => {
-    image.addEventListener('click', onProductCardImageClick);
+  image.addEventListener('click', onProductCardImageClick);
 });
 
-
-document.addEventListener('click', (evt) => {
-    if(evt.target === modal) {
-        evt.preventDefault;
-        closeModal();
-    }
-});
-
-document.addEventListener('keypress', (evt) => {
-    if(evt.key === 'Escape') { //FIXME
-        closeModal();
-    }
-})
-
-document.addEventListener('keypress', (evt) => {
-    if(evt.key === 'Enter') { //FIXME
-        closeModal();
-    }
-})
+//render main modal window
 
 const renderModalElement = (card) => {
-    const elem = document.createElement('div');
-    elem.insertAdjacentElement("beforeend", card);
-    return elem.firstElementChild;
+  const elem = document.createElement('div');
+  elem.insertAdjacentElement("beforeend", card);
+  return elem.firstChild;
 }
 
-const renderModalInfo = (item) => {
-    modal.innerHTML = "";
-    modal.insertAdjacentElement("beforeEnd", renderModalElement(getModalWindow(item)));
-    openModal();
+const renderModalInfo = (productData) => {
+  modal.innerHTML = "";
+  modal.insertAdjacentElement("beforeEnd", renderElement(getModalWindow(productData)));
 }
 
-renderModalInfo();
+// renderModalInfo();
 // renderModalInfo();
 
 // modal.innerHTML = "";
